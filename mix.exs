aea9615d2c076c4036a3d0e8a33b2aa544653f02
Stanislav Shandyga <ssh@anadeainc.com>
2021-03-27 19:33:33 +0200

    Upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Journal.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.2.0"},
+      {:bcrypt_elixir, "~> 2.3"},
       {:guardian, "~> 2.0"},
       {:phoenix_html_simplified_helpers, "~> 2.1.0"}
     ]
