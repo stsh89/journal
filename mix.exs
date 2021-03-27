61ff2ce007bc3e11a38db097fb481e0327d9d0a6
Stanislav Shandyga <ssh@anadeainc.com>
2021-03-27 11:24:05 +0200

    Add SSL to mix application

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Journal.MixProject do
   def application do
     [
       mod: {Journal.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
