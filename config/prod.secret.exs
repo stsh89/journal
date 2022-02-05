2bdd1dabdb6cf3faa5d9738b4c8842095bbcdc0c
Stanislav Shandyga <ssh@anadeainc.com>
2022-02-05 10:50:20 +0200

    Update pool size configuration with a new syntax

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :journal, Journal.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer("${POOL_SIZE}")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
